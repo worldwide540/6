3fcc23642b7373f09a38c87c38a97436137035b0
narek <narek@DESKTOP-98JS365>
2024-06-20 10:51:27 +0300

    Добавьте файлы проекта.

---
+++ 6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static bool IsPalindrome(string input)
+    {
+        input = input.ToLower(); // Приводим строку к нижнему регистру для учета регистра символов
+        int left = 0;
+        int right = input.Length - 1;
+
+        while (left < right)
+        {
+            if (input[left] != input[right])
+            {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        string testString = "radar";
+        Console.WriteLine($"Строка '{testString}' является палиндромом: {IsPalindrome(testString)}");
+    }
+}
+
